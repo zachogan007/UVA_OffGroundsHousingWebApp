cf80e65b36a21ada01179f757d2a188425e5daae
Thu Nguyen <thn8uym@virginia.edu>
2021-09-26 18:34:42 -0400

    added offGrounds app with initial index page

--- uvaOffGroundsHousing/urls.py
+++ uvaOffGroundsHousing/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import include, path
+
 urlpatterns = [
+    path('offGrounds/', include('offGrounds.urls')),
     path('admin/', admin.site.urls),
 ]
