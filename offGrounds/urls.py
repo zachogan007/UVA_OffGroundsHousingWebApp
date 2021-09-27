b32841bae22645a97bf27b6a651d71222225e13b
Thu Nguyen <thn8uym@virginia.edu>
2021-09-26 22:33:56 -0400

    finished skeleton code sprint 2

--- offGrounds/urls.py
+++ offGrounds/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+   # path('user/', views.show_user(), name='show_user'),
+   # path('review/', views.show_review(), name='show_review'),
 ]
