2634290397089fe50c62bcb7fd7ef0d72cc01fea
Michael Kosar <michaelkosar@Michaels-MBP-2.home>
2021-10-14 14:42:30 -0400

    google login

--- uvaOffGroundsHousing/settings.py
+++ uvaOffGroundsHousing/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'uvaOffGroundsHousing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'b-16',
+        'USER': 'postgres',
+        'PASSWORD': 'db3240uva',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
@@ -115,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
