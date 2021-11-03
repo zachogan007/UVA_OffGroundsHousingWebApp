42d4a2e4bb0524828bb7da58858a9fef7cdd98cb
nicCasco <nicoleicasco@gmail.com>
2021-11-03 17:45:07 -0400

    Modified models

--- offGrounds/models.py
+++ offGrounds/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
+class Listing(models.Model):
+    listing_name = models.CharField(max_length=200)
+    posting_date = models.DateTimeField('date published')
+    utilities = models.CharField(max_length=200)
+    beds = models.CharField(max_length = 200)
+    baths = models.CharField(max_length = 200)
+    parking = models.CharField(max_length = 200)
+
+
 # Create your models here.
 class Review(models.Model):
     review_text = models.TextField(max_length=20000)
