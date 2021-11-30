35d2c2532ee96b6f24535f9d3b3a9327c9641407
Zac Hogan <90006375+zachogan007@users.noreply.github.com>
2021-11-30 18:18:21 -0500

    small design updates

--- offGrounds/calendar.py
+++ offGrounds/calendar.py
@@ -15,7 +15,7 @@ class Calendar(HTMLCalendar):
 		d = ''
 		for event in events_per_day:
 			d += f'<li> {event.name} </li>'
-
+			
 		if day != 0:
 			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
 		return '<td></td>'
