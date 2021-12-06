f37c13862d93948034255a03a44cdfd7f0315b3a
jng9nha <77031897+jng9nha@users.noreply.github.com>
2021-12-05 19:35:58 -0500

    username in review

--- offGrounds/filters.py
+++ offGrounds/filters.py
@@ -1,14 +1,18 @@
 import django_filters
 from .models import *
+from django_filters import CharFilter, NumberFilter, NumericRangeFilter
 
 #Source: https://www.youtube.com/watch?v=G-Rct7Na0UQ
 class OrderFilter(django_filters.FilterSet):
+    address = CharFilter(field_name='address', lookup_expr='icontains')
+    rent = NumberFilter(field_name='rent', lookup_expr='lte')
     class Meta:
         model = Listing
         fields = '__all__'
         exclude = ['name', 'longitude', 'latitude', 'pub_date', 'image', 'laundry', 'parking', 'fitness']
 
 class ReviewFilter(django_filters.FilterSet):
+    content = CharFilter(field_name='content', lookup_expr='icontains')
     class Meta:
         model = Review
         fields = '__all__'
